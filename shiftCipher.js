bb4365f2291a46837806f5f91a67cdc81702effa
C0d3r15 <38169203+C0d3r15@users.noreply.github.com>
2018-05-01 00:51:15 -0400

    Add files via upload

---
+++ shiftCipher.js
@@ -0,0 +1,66 @@
+/*
+simple shift cipher or caesar cipher
+finish function to encrypt plaintext
+*/
+
+ var encrypt = (plainText) {
+ var crypt = ["g", "h", "i", "j", "k", "l", "m",
+ "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+ "x", "y", "z", "a", "b", "c", "d", "e", "f"]
+  for = (i = 0;i < plainText.lenght;i++){
+    if (plainText[i] = "a") {
+     console.log(crypt[i]);
+   } else if (plainText[i] = "b") {
+     console.log(crypt[1]);
+   } else if (plainText[i] = "c") {
+     console.log(crypt[2]);
+   } else if (plainText[i] = "d") {
+     console.log(crypt[3]);
+   } else if (plainText[i] = "e") {
+     console.log(crypt[4]);
+   } else if (plainText[i] = "f") {
+     console.log(crypt[5]);
+   } else if (plainText[i] = "g") {
+     console.log(crypt[6]);
+   } else if (plainText[i] = "h") {
+     console.log(crypt[7]);
+   } else if (plainText[i] = "i") {
+     console.log(crypt[8]);
+   } else if (plainText[i] = "j") {
+     console.log(crypt[9]);
+   } else if (plainText[i] = "k") {
+     console.log(crypt[10]);
+   } else if (plainText[i] = "l") {
+     console.log(crypt[11]);
+   } else if (plainText[i] = "m") {
+     console.log(crypt[12]);
+   } else if (plainText[i] = "n") {
+     console.log(crypt[13]);
+   } else if (plainText[i] = "o") {
+     console.log(crypt[14]);
+   } else if (plainText[i] = "p") {
+     console.log(crypt[15]);
+   } else if (plainText[i] = "q") {
+     console.log(crypt[16]);
+   } else if (plainText[i]) = "r" {
+     console.log(crypt[17]);
+   } else if (plainText[i] = "s") {
+     console.log(crypt[18]);
+   } else if (plainText[i] = "t") {
+     console.log(crypt[19]);
+   } else if (plainText[i] = "u") {
+     console.log(crypt[20]);
+   } else if (plainText[i] = "v") {
+     console.log(crypt[21]);
+   } else if (plainText[i] = "w") {
+     console.log(crypt[22]);
+   } else if (plainText[i] = "x") {
+     console.log(crypt[23]);
+   } else if (plainText[i] = "y") {
+     console.log(crypt[24]);
+   } else if (plainText[i] = "z") {
+     console.log(crypt[25]);
+   }
+};
+
+console.log(encrypt("hello world"));
